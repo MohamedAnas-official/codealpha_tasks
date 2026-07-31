d67a65b25387b3e82cb39e630d53fec2d2f3fe3f
M MOHAMED ANAS <crescentscc4@gmail.com>
2026-07-31 15:38:15 +0530

    Implement insecure login functionality

--- CodeAlpha_Secure Coding Review /insecure_login.py
+++ CodeAlpha_Secure Coding Review /insecure_login.py
@@ -1,1 +1,12 @@
+users = {
+    "admin": "admin123",
+    "user": "password"
+}
 
+username = input("Username: ")
+password = input("Password: ")
+
+if username in users and users[username] == password:
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
