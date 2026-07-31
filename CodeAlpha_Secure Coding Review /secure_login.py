4b4585cc34a5175001ef73ec8cb23e3a38cefd2e
M MOHAMED ANAS <crescentscc4@gmail.com>
2026-07-31 15:40:39 +0530

    Update secure_login.py

--- CodeAlpha_Secure Coding Review /secure_login.py
+++ CodeAlpha_Secure Coding Review /secure_login.py
@@ -1,1 +1,19 @@
+import hashlib
 
+users = {
+    "admin": hashlib.sha256("admin123".encode()).hexdigest(),
+    "user": hashlib.sha256("password".encode()).hexdigest()
+}
+
+username = input("Username: ").strip()
+password = input("Password: ").strip()
+
+hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+if username in users:
+    if users[username] == hashed_password:
+        print("Login Successful")
+    else:
+        print("Invalid Password")
+else:
+    print("User Not Found")
